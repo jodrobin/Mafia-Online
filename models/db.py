92f0023411a75e3b5dcbcc75f457a1005aa3095a
sajia <sajia@ucsc.edu>
2018-05-31 01:04:56 -0700

    changed look of welcome screen and login page; need to fix registeration and pass recovery pages

--- models/db.py
+++ models/db.py
@@ -80,6 +80,7 @@ mail.settings.ssl = myconf.get('smtp.ssl') or False
 auth.settings.registration_requires_verification = False
 auth.settings.registration_requires_approval = False
 auth.settings.reset_password_requires_verification = True
+auth.settings.remember_me_form = False
 
 # More API examples for controllers:
 #
