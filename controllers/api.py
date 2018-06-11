47899e9f52f166c86ba03912fd263114efc1c7ea
sajia <sajia@ucsc.edu>
2018-06-11 10:25:17 -0700

    fix some game joining bugs

--- controllers/api.py
+++ controllers/api.py
@@ -35,12 +35,9 @@ def get_users():
 
 def get_ingame_players():
     players = []
-    logger.info("get ingame players")
     game_id = db(db.player.user_email == auth.user.email).select().first().current_game
-    logger.info(game_id)
 
     for row in db(db.player.current_game == game_id).select():
-        logger.info(game_id)
         player = dict(
             role=row.role,
             initial_role = row.initial_role,
@@ -50,9 +47,8 @@ def get_ingame_players():
             bio=row.bio,
             username=row.username
         )
-        logger.info(player)
         players.append(player)
-    logger.info(players)
+
     return response.json(dict(
         players=players,
         user_id=auth.user.id,
@@ -106,28 +102,24 @@ def update_users():
         )
         logged_in_users.append(user)
 
-    logger.info(logged_in_users)
     return response.json(dict(
         users=logged_in_users,
     ))
 
 
 def send_msg():
-    logger.info(request.vars.chat_id)
     t_id = db.chat.insert(
         msg=request.vars.msg,
         author=request.vars.author,
         the_time=request.vars.the_time,
         chat_id=request.vars.chat_id,
     )
-    logger.info(t_id)
 
     return "ok"
 
 
 def get_new_msgs():
     messages = []
-    logger.info(request.vars.the_time)
     for row in db((db.chat.the_time >= request.vars.the_time) & (db.chat.chat_id == request.vars.chat_id)).select():
         message = dict(
             msg=row.msg,
@@ -135,16 +127,13 @@ def get_new_msgs():
         )
         messages.append(message)
 
-    logger.info(messages)
     return response.json(dict(
         messages=messages,
     ))
 
 
 def add_game():
-    logger.info(request.vars.new_game)
     t_id = db.game.insert(game_name=request.vars.new_game)
-    logger.info(t_id)
 
     row = db(db.player.id == request.vars.id).select().first()
     if row is not None:
@@ -157,7 +146,10 @@ def add_game():
 
 def get_games(): 
     games = []
-    for row in db(db.game.has_ended == 0).select(db.game.ALL):
+    logger.info("get the game")
+    for row in db((db.game.has_ended == 0) & (db.game.has_started == 0)).select(db.game.ALL):
+        logger.info("hi there")
+        logger.info(db.game.has_started)
         g = dict(
             game_name=row.game_name,
             num_players=row.num_players,
@@ -201,7 +193,6 @@ def leave_game():
                 has_ended=True
             )
         else:
-            logger.info(game_row.num_players)
             new_num_players = game_row.num_players - 1
             game_row.update_record(
                 num_players=new_num_players
@@ -220,21 +211,15 @@ def check_game():
 
 
 def join_game():
-    logger.info("joining game")
-    logger.info(request.vars.user_id)
-    logger.info(request.vars.game_id)
     player_row = db(db.player.id == request.vars.user_id).select().first()
     if player_row is not None:
-        logger.info("found user")
         player_row.update_record(
             current_game=request.vars.game_id
         )
 
     game_row = db(db.game.id == request.vars.game_id).select().first()
     if game_row is not None:
-        logger.info("found game")
         new_num_players = game_row.num_players + 1
-        logger.info(new_num_players)
         game_row.update_record(
             num_players=new_num_players
         )
